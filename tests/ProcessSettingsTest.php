373c4c3e1ca8c1be1df499a2947f14eb9cea7a11
Ryan Brooks <ryanbrooksis@gmail.com>
2016-11-16 22:31:25 +0000

    added comments

--- tests/ProcessSettingsTest.php
+++ tests/ProcessSettingsTest.php
@@ -7,7 +7,9 @@ class ProcessSettingsTest extends TestCase
     {
         require(dirname(__FILE__).'/../process_settings.php');
 
-        // Assert
+        // The user is alerted that the settings.php file is missing
         $this->expectOutputString("<div style='width:600px; background-color:#eee; padding:20px; font-family:arial;'><h3>settings.php file error</h3>Copy and modify default.settings.php to settings.php<br>For more information about configure settings.php file go to <a href=\"http://emoncms.org\">http://emoncms.org</a></div>");
+
+        // FIXME: assert that a die is triggered
     }
 }
